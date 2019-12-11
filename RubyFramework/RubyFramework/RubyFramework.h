8c5be213de0433509cce83ebe1e22aee99be6fa1
Rupinder Kaur <rupinderkaur@Rupinders-MacBook-Pro.local>
2019-12-11 15:54:47 -0500

    finished

---
+++ RubyFramework/RubyFramework/RubyFramework.h
@@ -0,0 +1,19 @@
+//
+//  RubyFramework.h
+//  RubyFramework
+//
+//  Created by Rupinder Kaur on 2019-12-11.
+//  Copyright © 2019 Rupinder Kaur. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RubyFramework.
+FOUNDATION_EXPORT double RubyFrameworkVersionNumber;
+
+//! Project version string for RubyFramework.
+FOUNDATION_EXPORT const unsigned char RubyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RubyFramework/PublicHeader.h>
+
+
---
+++ RubyFramework/RubyFramework/Service.swift
@@ -0,0 +1,20 @@
+//
+//  Service.swift
+//  RubyFramework
+//
+//  Created by Rupinder Kaur on 2019-12-11.
+//  Copyright © 2019 Rupinder Kaur. All rights reserved.
+//
+
+import Foundation
+
+public class Service{
+    
+    private init(){
+        
+    }
+    
+    public static func doSomething() -> String{
+        return "Did some stuff.Here you are"
+    }
+}
